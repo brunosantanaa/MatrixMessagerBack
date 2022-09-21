8d8c63eb771aad6eaa86ba430dde40ac0ce3cb28
Bruno de Santana <brunosantanaa@pop-os.localdomain>
2022-09-20 23:12:18 -0400

    feat: users list

---
+++ user/users/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../../db/controller.php');
+require_once('../../valide.php');
+
+  header("Content-Type: application/json");
+  // build a PHP variable from JSON sent using POST method
+  $v = json_decode(stripslashes(file_get_contents("php://input")));
+  if (isset($v->token)) {
+    $result = json_decode(is_valid($v));
+    if($result->access) {
+      $resp = sql_query('SELECT UserID, name, email FROM USERS;');
+      echo $resp;
+    } else {
+      echo json_encode($result);
+    }
+  }
+?>
